b0e58bd828b4835551596335c5a408c11285d2f2
Denis Tarasov <taras.baldej@gmail.com>
2022-09-22 21:47:16 +0300

    hero and  modal

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,45 @@
-$(function () {
+$( document ).ready(function () {
+  // mask
 
-})
+  $.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  };
+
+  $("#tel").click(function(){
+    $(this).setCursorPosition(4);
+  }).mask("+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9");
+
+  
+
+  $('.header__feedback').click(function() {
+    $('.modal').addClass('visible')
+  });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  // popup
+
+  document.querySelector('.header__feedback').addEventListener('click', () => {
+    document.querySelector('.modal').classList.add('visible');
+  });
+  //закрытие по области и крестику
+  document.querySelector('.modal').addEventListener('click', (e) => {
+    if (e.target.closest('.modal__close') || !e.target.closest('.modal__body')) {
+      document.querySelector('.modal').classList.remove('visible');
+    }
+  });
+  //закрытие по области по Esc
+  window.addEventListener('keydown', (e) => {
+    if (e.keyCode == 27) {
+      document.querySelector('.modal').classList.remove('visible');
+    }
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ function images() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery.maskedinput/src/jquery.maskedinput.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
